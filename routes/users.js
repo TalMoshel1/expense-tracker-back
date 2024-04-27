b2ba2c7eef514c73afb627818eef640a1ca48a5f
unknown <talmoshel444@gmail.com>
2024-04-27 23:15:40 +0300

    getting data

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,6 @@ johndoe3@example.com
 Password123!
 */
 router.post("/create", authController.createUser);
-router.get("/get", authController.getUser);
+router.post("/get", authController.getUser);
 
 module.exports = router;
