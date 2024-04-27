1ad61c1d0b5dc58f747c009272b4a76e21304d5b
unknown <talmoshel444@gmail.com>
2024-04-27 21:46:51 +0300

    update url

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const connectToDb = require('./db-helper/connectToDb.js');
-const usersRouter = require('./routes/users.js');
+const connectToDb = require("./db-helper/connectToDb.js");
+const usersRouter = require("./routes/users.js");
+const cors = require("cors");
 
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:3001",
+    })
+  );
+} else {
+  const allowedOrigins = ["https://expense-tracker-front-1.onrender.com"];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
+  app.use(cors(corsOptions));
+}
 
 connectToDb()
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`Server is running on http://localhost:${port}`);
-        });
-    })
-    .catch(error => {
-        console.error('Error connecting to DB:', error);
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
     });
-
+  })
+  .catch((error) => {
+    console.error("Error connecting to DB:", error);
+  });
 
 app.use(express.json());
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
 module.exports = { app };
